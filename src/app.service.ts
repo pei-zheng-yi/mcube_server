f57e7b4443e9bfdef891085432e0eea632d75ccc
peizhengyi <1195077416@qq.com>
2022-12-23 13:31:45 +0800

    feat: 增加UA工具函数

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,4 +1,6 @@
 import { Injectable } from '@nestjs/common';
+import { parseUserAgent } from './utils/ua.utils';
+import { UAParser } from 'ua-parser-js';
 
 @Injectable()
 export class AppService {
